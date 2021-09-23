26a55ccfd698a9817b1f51edc1d0e2548116eacd
WillianR381 <mynamewra@gmail.com>
2021-09-23 13:10:32 -0300

    desafio complementar

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
+  headers:{
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": true,
+          "Access-Control-Allow-Credentials": true,
+  }
 });
